f4d8084b993373e1bad6d7b87f7e11f84ce512d8
hyy0591 <yangyu.huang@gmail.com>
2023-10-14 21:09:56 +0800

    Update diy-part2.sh
    patch kernel to add mt7921k pci device id

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,16 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+cat > target/linux/x86/patches-5.15/199-add-mt7921k-pci-device-id << EOF
+--- a/drivers/net/wireless/mt76/mt7921/pci.c
++++ b/drivers/net/wireless/mt76/mt7921/pci.c
+@@ -14,6 +14,8 @@
+
+ static const struct pci_device_id mt7921_pci_device_table[] = {
+        { PCI_DEVICE(PCI_VENDOR_ID_MEDIATEK, 0x7961) },
++       { PCI_DEVICE(PCI_VENDOR_ID_MEDIATEK, 0x0608) },
++       { PCI_DEVICE(PCI_VENDOR_ID_MEDIATEK, 0x0616) },
+        { },
+ };
+EOF
