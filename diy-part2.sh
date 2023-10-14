4833743da81409f9f7e8a8f878ea9ea3c1d22811
hyy0591 <yangyu.huang@gmail.com>
2023-10-14 22:37:50 +0800

    Update diy-part2.sh
    fix source code path

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,8 +14,8 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 cat > target/linux/x86/patches-5.15/199-add-mt7921k-pci-device-id << EOF
---- a/drivers/net/wireless/mt76/mt7921/pci.c
-+++ b/drivers/net/wireless/mt76/mt7921/pci.c
+--- a/drivers/net/wireless/mediatek/mt76/mt7921/pci.c
++++ b/drivers/net/wireless/mediatek/mt76/mt7921/pci.c
 @@ -14,6 +14,8 @@
 
  static const struct pci_device_id mt7921_pci_device_table[] = {
